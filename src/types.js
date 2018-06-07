20430193554beaeee29cb425005c917037eada6e
jvke <hi@jvke.co>
2018-06-08 02:12:59 +1000

    some edits to types.js

--- src/types.js
+++ src/types.js
@@ -10,9 +10,36 @@ type CriticalResponses = {
 
 type Status = 'critical' | 'degraded' | 'ok' | 'unknown';
 
+type Device = 'android' | 'ios' | 'chrome' | 'firefox' | 'desktop';
+
+type ASN = {
+  value: string, // AS Number
+  amount?: number // amount of nodes to assign with this ASN
+};
+
+type GeoData = {
+  coords: {
+    lat: number,
+    lng: number,
+    bounds?: number // used for filtration later in roadmap. miners don't report this.
+  },
+  city?: string,
+  country: string,
+  [string]: string // this covers any other arbitrary address microdata
+};
+
+// 'amount' is spread across ASNs evenly unless "ASN.amount" is present,
+// in which case "Filters.amount" should == sum of all "ASN.amount" values.
+type Filters = {
+  amount?: number, // amount of nodes to perform job
+  ASN?: ASN[],
+  devices?: Device[], // hardware type of nodes to perform job
+};
+
 export type JobRequest = {
   name: string,
   type: string,
+  filters: Filters,
   protocol: string,
   headers: Headers,
   payload: string,
@@ -36,7 +63,7 @@ export type JobRequestMessage = {
   highWaterOffset: number,
   key: ?string,
   timestamp: Date
-}
+};
 
 export type JobResult = {
   result_uuid: string, // @TODO: subtype uuids as opaque data types
