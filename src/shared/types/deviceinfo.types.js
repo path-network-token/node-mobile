8aab9a1c127f1d6fe92de804b4a0987762dc4c9a
Joshua Keating <himself@joshs.space>
2018-06-12 14:07:25 +1000

    MM-7: updating types

---
+++ src/shared/types/deviceinfo.types.js
@@ -0,0 +1,8 @@
+// @flow
+export type DeviceInfo = {
+  deviceId: string,
+  ip: string,
+  asn: string,
+  lat: string,
+  lng: string
+};
--- src/shared/types/storage.types.js
+++ src/shared/types/storage.types.js
@@ -1,5 +1,4 @@
 // @flow
-
 export type PersistantStorage = {
   // settings
   walletAddress: string,
