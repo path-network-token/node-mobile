284c0114e0b766dd4fa390ecd34ce3391b1d2205
Joshua Keating <himself@joshs.space>
2018-06-12 15:19:35 +1000

    MM-7: Updated info boxes to use data from redux store

--- src/containers/Info.js
+++ src/containers/Info.js
@@ -1,33 +1,36 @@
 // @flow
 import * as React from 'react';
 import { View } from 'react-native';
+import { connect } from 'react-redux';
 
 import InfoBox from '../components/InfoBox';
 
-type Props = {};
-
-type State = {
-  jobs: number,
-  tokens: number,
-  status: string
+type Props = {
+  jobCompleteCount: number,
+  pathMinedCount: number,
+  jobStatus: string
 };
 
-export default class Info extends React.Component<Props, State> {
-  state = {
-    jobs: 0,
-    tokens: 0,
-    status: 'Idle'
-  };
-
+class Info extends React.Component<Props> {
   render() {
-    const { jobs, tokens, status } = this.state;
+    const { jobCompleteCount, pathMinedCount, jobStatus } = this.props;
 
     return (
       <View>
-        <InfoBox prefix="Jobs completed: " data={jobs} />
-        <InfoBox prefix="Path mined: " data={tokens} />
-        <InfoBox prefix="Status: " data={status} />
+        <InfoBox prefix="Jobs completed: " data={jobCompleteCount} />
+        <InfoBox prefix="Path mined: " data={pathMinedCount} />
+        <InfoBox prefix="Status: " data={jobStatus} />
       </View>
     );
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    jobCompleteCount: state.jobCompleteCount,
+    pathMinedCount: state.pathMinedCount,
+    jobStatus: state.jobStatus
+  };
+};
+
+export default connect(mapStateToProps)(Info);
