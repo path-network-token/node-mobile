76eb43295976b4bea642c3c3825fb1c669f286c6
Joshua Keating <himself@joshs.space>
2018-06-12 14:08:32 +1000

    MM-7: commenting out browser extension functionality

--- src/background/index.js
+++ src/background/index.js
@@ -3,30 +3,21 @@ import { AsyncStorage } from 'react-native';
 
 import io from 'socket.io-client';
 
-import configureStore from '../shared/store/configureStore';
-import initStorage from '../shared/initStorage';
-import createInitState from '../shared/helpers/createInitState';
-
-const storage = JSON.parse(AsyncStorage.getItem('persistent')) || initStorage;
-const initialState = createInitState(storage);
-
-const store = configureStore(initialState);
-
 const socket = io('http://nodejs.nyc.path.network:3000/path');
 
-// eslint-disable-next-line
-chrome.runtime.onMessage.addListener((req, sender, sendResponse) => {
-  if (req.action === 'updateState') {
-    store.dispatch({
-      type: 'UPDATE_STATE',
-      state: req.state
-    });
-  }
-
-  if (req.action === 'getState') {
-    sendResponse(store.getState());
-  }
-});
+// // eslint-disable-next-line
+// chrome.runtime.onMessage.addListener((req, sender, sendResponse) => {
+//   if (req.action === 'updateState') {
+//     store.dispatch({
+//       type: 'UPDATE_STATE',
+//       state: req.state
+//     });
+//   }
+//
+//   if (req.action === 'getState') {
+//     sendResponse(store.getState());
+//   }
+// });
 
 socket.on('connect', () => {
   const index = socket.io.engine.upgrade ? 1 : 0;
