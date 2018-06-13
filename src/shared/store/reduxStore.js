4048662b2392dc6ccf319093884af884b66a4b8e
jvke <hi@jvke.co>
2018-06-13 17:21:36 +1000

    Fixed a few LOCs ;)

--- src/shared/store/reduxStore.js
+++ src/shared/store/reduxStore.js
@@ -9,13 +9,8 @@ import initStorage from './initStorage';
 import createInitState from './createInitState';
 
 const reduxStore = async () => {
-  let storage = await AsyncStorage.getItem('persistent');
-
-  if (storage == null) {
-    storage = initStorage;
-  }
-
-  const initialState = createInitState(storage);
+  const storage = await AsyncStorage.getItem('persistent');
+  const initialState = createInitState(storage || initStorage);
   const middlewares = [thunk];
 
   return createStore(reducers, initialState, applyMiddleware(...middlewares));
