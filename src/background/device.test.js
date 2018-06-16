cc4282c830171cc097f6264007842a35f80c2a4c
Joshua Keating <himself@joshs.space>
2018-06-16 18:17:11 +1000

    MM-7: Removed background folder

--- src/background/device.test.js
+++
@@ -1,7 +0,0 @@
-import expect from 'expect';
-
-describe('background: device tests', () => {
-  it('Empty Test', () => {
-    expect(true).toEqual(true);
-  });
-});
--- src/background/jobs.test.js
+++
@@ -1,7 +0,0 @@
-import expect from 'expect';
-
-describe('background: jobs tests', () => {
-  it('Empty Test', () => {
-    expect(true).toEqual(true);
-  });
-});
