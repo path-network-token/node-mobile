9fec297bb936f7ffdf5b0a57faf66684c065df09
Haydart <makowiecki.radoslaw@gmail.com>
2018-09-17 15:18:31 +0200

    Create base activity class

---
+++ app/src/main/java/network/path/mobilenode/BaseActivity.kt
@@ -0,0 +1,16 @@
+package network.path.mobilenode
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    protected abstract val viewModel: ViewModel
+    protected abstract val layoutResId: Int
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(layoutResId)
+    }
+}
