8f09a99580f8a03b21f96272a8c0564b6e48a2b8
Haydart <makowiecki.radoslaw@gmail.com>
2018-09-19 16:00:06 +0200

    Add wallet address validation

--- app/src/main/java/network/path/mobilenode/ui/main/wallet/WalletFragment.kt
+++ app/src/main/java/network/path/mobilenode/ui/main/wallet/WalletFragment.kt
@@ -2,12 +2,13 @@ package network.path.mobilenode.ui.main.wallet
 
 import android.os.Bundle
 import android.view.View
-import android.widget.EditText
 import kotlinx.android.synthetic.main.fragment_wallet.*
 import network.path.mobilenode.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.util.regex.Pattern
 
 private const val WALLET_ADDRESS_MAX_LINES = 2 //not working in XML - workaround
+private const val ETH_ADDRESS_PATTERN = "^0x[a-fA-F0-9]{40}\$"
 
 class WalletFragment : BaseFragment() {
 
@@ -26,17 +27,40 @@ class WalletFragment : BaseFragment() {
             setHorizontallyScrolling(false)
             maxLines = WALLET_ADDRESS_MAX_LINES
             setText(storage.pathWalletAddress)
-            onTextChanged { onWalletAddressConfirmed() }
+            onTextChanged { onWalletAddressChanged() }
+        }
+
+        linkWalletButton.setOnClickListener { onLinkWalletAddressButtonClicked() }
+    }
+
+    private fun onWalletAddressChanged() {
+        with(walletAddressInputEditText) {
+            val walletAddressValidationError = when {
+                text.toString().isBlank() -> {
+                    linkWalletButton.isEnabled = false
+                    getString(R.string.blank_path_wallet_address_error)
+                }
+                isValidWalletAddress(text.toString()) -> {
+                    linkWalletButton.isEnabled = true
+                    null
+                }
+                else -> {
+                    linkWalletButton.isEnabled = false
+                    getString(R.string.invalid_path_wallet_address_error)
+                }
+            }
+
+            walletAddressInputLayout.error = walletAddressValidationError
         }
     }
 
-    private fun onWalletAddressConfirmed() {
-        if ((walletAddressInputEditText as EditText).text.isBlank()) {
-            walletAddressInputEditText.error = getString(R.string.blank_path_wallet_address_error)
-        } else {
-            updatePathWalletAddress()
-            walletAddressInputEditText.error = null
-        }
+    private fun isValidWalletAddress(text: CharSequence) = Pattern
+        .compile(ETH_ADDRESS_PATTERN)
+        .matcher(text)
+        .matches()
+
+    private fun onLinkWalletAddressButtonClicked() {
+        updatePathWalletAddress()
     }
 
     private fun updatePathWalletAddress() {
