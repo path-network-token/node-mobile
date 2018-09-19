5efbc169f1afa937f6b011395c3eff07cbcd7daf
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2018-09-19 12:57:21 +0200

    [PAN-7] Add Job report layout (#8)

---
+++ app/src/main/java/network/path/mobilenode/ui/main/jobreport/LatencyChart.kt
@@ -0,0 +1,29 @@
+package network.path.mobilenode.ui.main.jobreport
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.res.use
+import kotlinx.android.synthetic.main.latency_chart.view.*
+import network.path.mobilenode.R
+
+class LatencyChart @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0, defStyleRes: Int = 0) :
+    ConstraintLayout(context, attrs, defStyleAttr) {
+
+    init {
+        inflate(context, R.layout.latency_chart, this)
+
+        context.obtainStyledAttributes(attrs, R.styleable.LatencyChart, defStyleAttr, defStyleRes).use {
+            val titleText = it.getString(R.styleable.LatencyChart_label)
+            val progressMillis = it.getInt(R.styleable.LatencyChart_progress_millis, 0)
+            val maxMillis = it.getInt(R.styleable.LatencyChart_max_millis, 0)
+            val progressSeconds = progressMillis / 1000
+
+            latencyChartLabelTextView.text = titleText
+            latencyValueTextView.text = context.getString(R.string.latency_chart_value, progressSeconds)
+            latencyProgressBar.progress = progressMillis
+            latencyProgressBar.max = maxMillis
+
+        }
+    }
+}
