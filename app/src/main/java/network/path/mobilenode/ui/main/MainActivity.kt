dea2abd77b6b8007ad0b022ea8ea13d4557f0a6f
lkopocinski <lkopocinski@gmail.com>
2018-09-25 12:13:48 +0200

    Add Navigation Graph

--- app/src/main/java/network/path/mobilenode/ui/main/MainActivity.kt
+++ app/src/main/java/network/path/mobilenode/ui/main/MainActivity.kt
@@ -1,60 +1,11 @@
 package network.path.mobilenode.ui.main
 
-import android.os.Bundle
-import androidx.fragment.app.transaction
-import kotlinx.android.synthetic.main.activity_main.*
 import network.path.mobilenode.BaseActivity
 import network.path.mobilenode.R
-import network.path.mobilenode.ui.main.dashboard.DashboardFragment
-import network.path.mobilenode.ui.main.wallet.WalletFragment
 import org.koin.android.architecture.ext.android.viewModel
 
 class MainActivity : BaseActivity() {
 
     override val layoutResId = R.layout.activity_main
     override val viewModel by viewModel<MainViewModel>()
-
-    private val walletFragment by lazy { WalletFragment() }
-    private val dashboardFragment by lazy { DashboardFragment() }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        if (savedInstanceState == null) {
-            dashboardRadioButton.isChecked = true
-            initFragments()
-        }
-        initBottomBar()
-    }
-
-    private fun initFragments() {
-        supportFragmentManager.transaction {
-            val containerResId = R.id.fragmentContainer
-            add(containerResId, dashboardFragment)
-        }
-    }
-
-    private fun initBottomBar() {
-        walletRadioButton.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                showWalletFragment()
-            }
-        }
-        dashboardRadioButton.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                showDashboardFragment()
-            }
-        }
-    }
-
-    private fun showWalletFragment() {
-        supportFragmentManager.transaction {
-            replace(R.id.fragmentContainer, walletFragment)
-        }
-    }
-
-    private fun showDashboardFragment() {
-        supportFragmentManager.transaction {
-            replace(R.id.fragmentContainer, dashboardFragment)
-        }
-    }
 }
---
+++ app/src/main/java/network/path/mobilenode/ui/main/MainFragment.kt
@@ -0,0 +1,63 @@
+package network.path.mobilenode.ui.main
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.transaction
+import kotlinx.android.synthetic.main.fragment_main.*
+
+import network.path.mobilenode.BaseFragment
+import network.path.mobilenode.R
+import network.path.mobilenode.ui.main.dashboard.DashboardFragment
+import network.path.mobilenode.ui.main.wallet.WalletFragment
+
+class MainFragment : BaseFragment() {
+
+    override val viewModel = MainFragmentViewModel()
+    override val layoutResId = R.layout.fragment_main
+
+    private val walletFragment by lazy { WalletFragment() }
+    private val dashboardFragment by lazy { DashboardFragment() }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if (savedInstanceState == null) {
+            dashboardRadioButton.isChecked = true
+            initFragments()
+        }
+        initBottomBar()
+    }
+
+    private fun initFragments() {
+        activity!!.supportFragmentManager.transaction {
+            val containerResId = R.id.fragmentContainer
+            add(containerResId, dashboardFragment)
+        }
+    }
+
+    private fun initBottomBar() {
+        walletRadioButton.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                showWalletFragment()
+            }
+        }
+        dashboardRadioButton.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                showDashboardFragment()
+            }
+        }
+    }
+
+    private fun showWalletFragment() {
+        activity!!.supportFragmentManager.transaction {
+            replace(R.id.fragmentContainer, walletFragment)
+        }
+    }
+
+    private fun showDashboardFragment() {
+        activity!!.supportFragmentManager.transaction {
+            replace(R.id.fragmentContainer, dashboardFragment)
+        }
+    }
+}
