e9a21ea9b8feb96d5b57dd84482796b9eaebc00a
lkopocinski <lkopocinski@gmail.com>
2018-09-25 13:44:03 +0200

    Show splash screen

--- app/src/main/java/network/path/mobilenode/ui/splash/SplashFragment.kt
+++ app/src/main/java/network/path/mobilenode/ui/splash/SplashFragment.kt
@@ -1,9 +1,24 @@
 package network.path.mobilenode.ui.splash
 
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import androidx.navigation.fragment.NavHostFragment
 import network.path.mobilenode.BaseFragment
 import network.path.mobilenode.R
+
 
 class SplashFragment : BaseFragment() {
 
     override val layoutResId: Int = R.layout.fragment_splash
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Handler().postDelayed(
+                {
+                    NavHostFragment.findNavController(this)
+                            .navigate(R.id.action_splashFragment_to_mainFragment)
+                }, 5000)
+    }
 }
