c1f392287a01b1c664a438592e7e9ba8d3d7d064
lkopocinski <lkopocinski@gmail.com>
2018-09-26 15:36:51 +0200

    Prepare methods to fill the dashboard view

---
+++ app/src/main/java/network/path/mobilenode/ui/main/dashboard/DashboardDetailsViewState.kt
@@ -0,0 +1,8 @@
+package network.path.mobilenode.ui.main.dashboard
+
+data class DashboardDetailsViewState(
+    val operatorAsn: String,
+    val autonomousService: String,
+    val country: String,
+    val deviceType: String
+)
--- app/src/main/java/network/path/mobilenode/ui/main/dashboard/DashboardFragment.kt
+++ app/src/main/java/network/path/mobilenode/ui/main/dashboard/DashboardFragment.kt
@@ -2,6 +2,10 @@ package network.path.mobilenode.ui.main.dashboard
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.dashboard_details.*
+import kotlinx.android.synthetic.main.fragment_dashboard.*
+import kotlinx.android.synthetic.main.view_job_report_button.*
 import network.path.mobilenode.BaseFragment
 import network.path.mobilenode.R
 
@@ -11,10 +15,33 @@ class DashboardFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        setupClicks()
+        setNodeId(10001)
     }
 
-    private fun setupViews() {
+    private fun setupClicks() {
+        viewJobReportButton.setOnClickListener {
+            // TODO: PAN-7
+        }
+    }
 
+    private fun setNodeId(nodeId: Int) {
+        nodeIdTextView.text = getString(R.string.node_id, nodeId)
+    }
+
+    private fun colorConnectionStatusDot(isConnected: Boolean) {
+        val colorRes = if (isConnected) R.color.apple_green else R.color.coral_pink
+        connectionStatusDot.drawable.setTint(
+            ContextCompat.getColor(context!!, colorRes)
+        )
+    }
+
+    private fun setDashboardDetails(details: DashboardDetailsViewState) {
+        operatorAsn.text = details.operatorAsn
+        autonomousService.text = details.autonomousService
+        country.text = details.country
+        deviceType.text = details.deviceType
     }
 
     companion object {
