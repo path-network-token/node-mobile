e232e2343a8cf4575443e509a34be0440cec2de3
lkopocinski <lkopocinski@gmail.com>
2018-09-27 12:39:28 +0200

    Prepare UI logic listeners

--- app/src/main/java/network/path/mobilenode/ui/main/jobreport/JobReportFragment.kt
+++ app/src/main/java/network/path/mobilenode/ui/main/jobreport/JobReportFragment.kt
@@ -1,9 +1,40 @@
 package network.path.mobilenode.ui.main.jobreport
 
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.fragment_job_report.*
+import kotlinx.android.synthetic.main.job_types_layout.*
 import network.path.mobilenode.BaseFragment
 import network.path.mobilenode.R
 
 class JobReportFragment : BaseFragment() {
 
     override val layoutResId = R.layout.fragment_job_report
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupCloseScreenButton()
+        setupJobTypesClickListeners()
+    }
+
+    private fun setupCloseScreenButton() {
+        closeScreenImageView.setOnClickListener {
+            activity!!.onBackPressed()
+        }
+    }
+
+    private fun setupJobTypesClickListeners() {
+        httpChecksButton.setOnClickListener {
+            // TODO: PAN-8
+        }
+
+        dnsChecksButton.setOnClickListener {
+            // TODO: PAN-8
+        }
+
+        customChecksButton.setOnClickListener {
+            // TODO: PAN-8
+        }
+    }
 }
