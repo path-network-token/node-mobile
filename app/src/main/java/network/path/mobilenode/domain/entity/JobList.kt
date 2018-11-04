293d241bd458880748deb5eb3c293d67e2f68e44
Kirill Kononovich <kirill@openwealth.com.au>
2018-11-05 09:47:54 +1100

    Consume new fields returned by API

--- app/src/main/java/network/path/mobilenode/domain/entity/JobList.kt
+++ app/src/main/java/network/path/mobilenode/domain/entity/JobList.kt
@@ -3,6 +3,9 @@ package network.path.mobilenode.domain.entity
 data class JobList(
         val type: String,
         val nodeId: String?,
+        val asn: String?,
+        val networkPrefix: String?,
+        val location: String?,
         val jobs: List<JobExecutionId>
 )
 
