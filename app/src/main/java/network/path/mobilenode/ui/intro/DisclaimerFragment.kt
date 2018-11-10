12d85e200617e3f6330327e63939d0af96457b4b
Kirill Kononovich <kirill@openwealth.com.au>
2018-11-10 23:23:22 +1100

    Disclaimer body placeholder with clickable link

--- app/src/main/java/network/path/mobilenode/ui/intro/DisclaimerFragment.kt
+++ app/src/main/java/network/path/mobilenode/ui/intro/DisclaimerFragment.kt
@@ -1,11 +1,15 @@
 package network.path.mobilenode.ui.intro
 
 import android.os.Bundle
-import android.text.method.ScrollingMovementMethod
+import android.text.method.LinkMovementMethod
 import android.view.View
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.fragment_disclaimer.*
 import network.path.mobilenode.R
 import network.path.mobilenode.ui.base.BaseFragment
+import network.path.mobilenode.utils.launchUrl
+import network.path.mobilenode.utils.setTextWithLinks
+
 
 class DisclaimerFragment : BaseFragment() {
     override val layoutResId = R.layout.fragment_disclaimer
@@ -18,7 +22,12 @@ class DisclaimerFragment : BaseFragment() {
     }
 
     private fun setupDisclaimerBody() {
-        disclaimerBodyTextView.movementMethod = ScrollingMovementMethod()
+        val context = context ?: return
+        disclaimerBodyTextView.highlightColor = ContextCompat.getColor(context, android.R.color.transparent)
+        disclaimerBodyTextView.setTextWithLinks(getString(R.string.disclaimer_body)) {
+            context.launchUrl(it)
+        }
+        disclaimerBodyTextView.movementMethod = LinkMovementMethod.getInstance()
     }
 
     private fun setupCloseScreenButton() {
