71faaf0005c42ff8e96e99591b94bde44d9a1433
Kirill Kononovich <azazel.kon@gmail.com>
2018-11-16 15:35:48 +1100

    Extract common interface from splash and about screens

--- app/src/main/java/network/path/mobilenode/ui/splash/SplashFragment.kt
+++ app/src/main/java/network/path/mobilenode/ui/splash/SplashFragment.kt
@@ -2,7 +2,6 @@ package network.path.mobilenode.ui.splash
 
 import android.os.Bundle
 import android.view.View
-import androidx.navigation.fragment.FragmentNavigator
 import androidx.navigation.fragment.NavHostFragment
 import kotlinx.android.synthetic.main.fragment_splash.*
 import network.path.mobilenode.BuildConfig
@@ -32,15 +31,10 @@ class SplashFragment : BaseFragment() {
     }
 
     private fun showScreen(nextScreen: SplashViewModel.NextScreen) {
-        val builder = FragmentNavigator.Extras.Builder()
         val actionId = when (nextScreen) {
             SplashViewModel.NextScreen.INTRO -> R.id.action_splashFragment_to_introFragment
-            SplashViewModel.NextScreen.MAIN -> {
-                builder.addSharedElement(imageGlobe, imageGlobe.transitionName)
-                R.id.action_splashFragment_to_mainFragment
-            }
+            SplashViewModel.NextScreen.MAIN -> R.id.action_splashFragment_to_mainFragment
         }
-        NavHostFragment.findNavController(this)
-                .navigate(actionId, null, null, builder.build())
+        NavHostFragment.findNavController(this).navigate(actionId)
     }
 }
