1c5e9fb6cab14ba39b1c19d5c0b4e18e73ee9860
Kirill Kononovich <azazel.kon@gmail.com>
2018-11-22 13:07:29 +1100

    Improve DGA

--- app/src/main/java/network/path/mobilenode/service/DomainGenerator.kt
+++ app/src/main/java/network/path/mobilenode/service/DomainGenerator.kt
@@ -20,10 +20,9 @@ object DomainGenerator : CoroutineScope {
     override val coroutineContext: CoroutineContext
         get() = Dispatchers.IO + job
 
+    private const val CHECK_MAX_DAYS = 10
     private val SEED = listOf(
-            intArrayOf(17, 8, 11, 16, 4, 25, 12, 13, 19, 5, 11, 41),
-            intArrayOf(8, 11, 17, 4, 25, 16, 13, 19, 12, 7, 14, 47),
-            intArrayOf(7, 12, 15, 6, 23, 19, 7, 15, 31, 3, 17, 59)
+            intArrayOf(8, 11, 17, 4, 25, 16, 13, 19, 12, 7, 14, 47)
     )
 
     private fun generateDomains(): Set<String> {
@@ -33,7 +32,13 @@ object DomainGenerator : CoroutineScope {
         cal.time = date
 
         return SEED.fold(mutableSetOf()) { set, seed ->
-            set.addAll(generate(seed, cal))
+            (0 until CHECK_MAX_DAYS).fold(set) { innerSet, _ ->
+                val newSet = generate(seed, cal)
+                cal.add(Calendar.DAY_OF_YEAR, -1)
+                Timber.d("DOMAIN: ${cal.time} => ${newSet.size}")
+                innerSet.addAll(newSet)
+                innerSet
+            }
             set
         }
     }
@@ -45,20 +50,21 @@ object DomainGenerator : CoroutineScope {
         var hour = it.toLong()
         val domain = StringBuffer("http://")
         for (i in 1..16) {
-            year = ((year xor seed[0] * year) shr seed[1]) xor ((year and 0xFFFFFFF0) shl seed[2])
-            month = ((month xor seed[3] * month) shr seed[4]) xor seed[5] * (month and 0xFFFFFFF8)
-            day = ((day xor (day shl seed[6])) shr seed[7]) xor ((day and 0xFFFFFFFE) shl seed[8])
-            hour = ((hour xor seed[9] * hour) shr seed[10]) xor (hour shl seed[11])
+            year = ((year xor seed[0] * year) shr seed[1]) xor (year shl seed[2])
+            month = ((month xor seed[3] * month) shr seed[4]) xor (seed[5] * month)
+            day = ((day xor (day shl seed[6])) shr seed[7]) xor (day shl seed[8])
+            hour = ((hour xor (hour shl seed[9])) shr seed[10]) xor (hour shl seed[11])
             val char = ((abs(year xor month xor day xor hour) % 25) + 97).toChar()
             domain.append(char)
         }
         domain.append(".net")
         domain.toString()
-    }
+    }.toSet()
 
     fun findDomain(): String? {
         job = Job()
         val domains = generateDomains()
+        // Timber.d("DOMAIN: potential domains [${domains.joinToString(separator = "\n")}]")
         Timber.d("DOMAIN: potential domains count [${domains.size}]")
         val resolved = runBlocking {
             domains.mapNotNull {
