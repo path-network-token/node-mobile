fddc8d0585434c1cccdaf9908074917117012280
Kirill Kononovich <azazel.kon@gmail.com>
2018-12-19 16:38:06 +1100

    Add LOOKING connect status to implement loading UI in dashboard

--- app/src/main/java/network/path/mobilenode/data/http/PathHttpEngine.kt
+++ app/src/main/java/network/path/mobilenode/data/http/PathHttpEngine.kt
@@ -65,7 +65,7 @@ class PathHttpEngine(
     override val coroutineContext: CoroutineContext
         get() = Dispatchers.IO + job
 
-    override val status = ConflatedBroadcastChannel(ConnectionStatus.DISCONNECTED)
+    override val status = ConflatedBroadcastChannel(ConnectionStatus.LOOKING)
     override val requests = ConflatedBroadcastChannel<JobRequest>()
     override val nodeId = ConflatedBroadcastChannel(storage.nodeId)
     override val jobList = ConflatedBroadcastChannel<JobList>()
